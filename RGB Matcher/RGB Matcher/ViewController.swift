5218f71e07bdf0317c108b6af356ad2d49be073a
Emmett Shaughnessy <github@emmetts.dev>
2021-09-30 09:21:58 -0500

    init

---
+++ RGB Matcher/RGB Matcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB Matcher
+//
+//  Created by Emmett Shaughnessy on 9/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
